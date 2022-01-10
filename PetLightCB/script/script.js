8bf57f4f07425d7d6a11cd273d4e53166159f84f
Renato Azevedo <renato.azevedo0001@gmail.com>
2022-01-10 08:19:46 -0300

    Initial commit

---
+++ PetLightCB/script/script.js
@@ -0,0 +1,11 @@
+/* Parte do Menu */
+
+
+function concordar() {
+
+    var element1 = document.querySelector(".politicaprivacidade");
+    console.log(element1);
+    element1.classList.remove("politicaprivacidade")
+    console.log(element1)
+    element1.classList.add("politicaprivacidadeaceita")
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projetos-HTML-CSS-JS
+ Projetos em HTML CSS e JS
